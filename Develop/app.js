2b6d21a5463847da76a5e37d1a378faf6366ae35
Will Kendall <will@wills-MacBook-Pro.local>
2020-09-29 19:04:08 -0700

    finished inquirer prompts for all classes, created fs.writeFileSync top export team array

--- Develop/app.js
+++ Develop/app.js
@@ -39,7 +39,7 @@ function createManager() {
             name: "addAnotherEmployee",
             type: "list",
             message: "Choose another employee type: ",
-            choices: ["Engineer","Intern","No more members to add"]
+            choices: ["Manager","Engineer","Intern","No more members to add"]
         }
     ]).then( response => {
         
@@ -48,6 +48,9 @@ function createManager() {
         teamMemberArray.push(manager);
 
         switch (response.addAnotherEmployee){
+            case "Manager":
+                createManager();
+                break;
             case "Engineer":
                 createEngineer();
                 break;
@@ -58,8 +61,6 @@ function createManager() {
                 makeTemplate();
                 break;    
         }
-        //testing response w/ console.log
-        console.log(response)
     })
 }
 
@@ -72,38 +73,105 @@ function createEngineer(){
             name: "engineerName",
             type: "input",
             message: "Enter a name for engineer: "
+        },
+        {
+            name: "engineerId",
+            type: "input",
+            message: "Enter an id for engineer: "
+        },
+        {
+            name: "engineerEmail",
+            type: "input",
+            message: "Enter an email for engineer: "
+        },
+        {
+            name: "gitHub",
+            type: "input",
+            message: "Enter a github for engineer: "
+        },
+        {
+            name: "addAnotherEmployee",
+            type: "list",
+            message: "Choose another employee type: ",
+            choices: ["Manager","Engineer","Intern","No more members to add"]
         }
-    ])
+    ]).then( response => {
+        
+        let engineer = new Engineer(response.engineerName,response.engineerId,response.engineerEmail,response.gitHub);
+
+        teamMemberArray.push(engineer);
+
+        switch (response.addAnotherEmployee){
+            case "Manager":
+                createManager();
+                break;
+            case "Engineer":
+                createEngineer();
+                break;
+            case "Intern":
+                createIntern();
+                break;
+            default:
+                makeTemplate();
+                break;    
+        }
+    })
+}
+
+function createIntern(){
+
+    inquirer.prompt([
+        {
+            name: "internName",
+            type: "input",
+            message: "Enter a name for intern: "
+        },
+        {
+            name: "internId",
+            type: "input",
+            message: "Enter an id for intern: "
+        },
+        {
+            name: "internEmail",
+            type: "input",
+            message: "Enter an email for intern: "
+        },
+        {
+            name: "school",
+            type: "input",
+            message: "Enter a school for intern: "
+        },
+        {
+            name: "addAnotherEmployee",
+            type: "list",
+            message: "Choose another employee type: ",
+            choices: ["Manager","Engineer","Intern","No more members to add"]
+        }
+    ]).then( response => {
+        
+        let intern = new Intern(response.internName,response.internId,response.internEmail,response.school);
+
+        teamMemberArray.push(intern);
+
+        switch (response.addAnotherEmployee){
+            case "Manager":
+                createManager();
+                break;
+            case "Engineer":
+                createEngineer();
+                break;
+            case "Intern":
+                createIntern();
+                break;
+            default:
+                makeTemplate();
+                break;    
+        }
+    })
 }
 
 function makeTemplate(){
 
-    fs.writeFile(outputPath,render(teamMemberArray),"utf-8");
-
+    fs.writeFileSync(outputPath,render(teamMemberArray),"utf-8")
+ 
 }
-
-//test app.js
-//make sure htmlrenderer.js is correct 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,4 +1,5 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// Defines the "Employee" super class
+
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
